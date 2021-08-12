cf9255cdeb6874ae0119174d2f096b9683fc281e
janicecaldeira <janicecaldeira@gmail.com>
2021-08-11 22:33:14 -0300

    Atualizando JS áudio

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,7 @@ let card = document.querySelectorAll(".card")
 
 for (let c = 0; c < card.length; c++) {
     card[c].addEventListener('mouseover', () => {
+        audio[c].currentTime = 0
         audio[c].play()
         audio[c].volume = 0.3
     })
